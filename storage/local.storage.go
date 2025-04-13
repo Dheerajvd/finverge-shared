593cc244e8eb94da3794f2e7abc7740805c0b048
Dheeraj Dalabanjan <dheeraj.dalabanjan@outlook.com>
2025-04-13 16:11:06 +0530

    [FIX] S3 fixes

--- storage/local.storage.go
+++ storage/local.storage.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"mime/multipart"
 	"os"
 	"path/filepath"
@@ -12,6 +13,7 @@ type LocalUploader struct {
 	BasePath string
 }
 
+// Upload saves the file locally under BasePath
 func (l *LocalUploader) Upload(file io.Reader, fileHeader *multipart.FileHeader) (string, error) {
 	workingDir, err := os.Getwd()
 	if err != nil {
@@ -35,4 +37,45 @@ func (l *LocalUploader) Upload(file io.Reader, fileHeader *multipart.FileHeader)
 	}
 
 	return filePath, nil
+}
+
+// Get opens a file for reading
+func (l *LocalUploader) Get(filename string) (io.ReadCloser, error) {
+	fullPath := filepath.Join(l.BasePath, filename)
+	return os.Open(fullPath)
+}
+
+// List returns all files under BasePath
+func (l *LocalUploader) List() ([]string, error) {
+	files, err := ioutil.ReadDir(l.BasePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list files: %v", err)
+	}
+
+	var filenames []string
+	for _, f := range files {
+		if !f.IsDir() {
+			filenames = append(filenames, f.Name())
+		}
+	}
+	return filenames, nil
+}
+
+// Delete removes a single file
+func (l *LocalUploader) Delete(filename string) error {
+	fullPath := filepath.Join(l.BasePath, filename)
+	if err := os.Remove(fullPath); err != nil {
+		return fmt.Errorf("failed to delete file: %v", err)
+	}
+	return nil
+}
+
+// DeleteMultiple removes multiple files
+func (l *LocalUploader) DeleteMultiple(filenames []string) error {
+	for _, name := range filenames {
+		if err := l.Delete(name); err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- storage/s3.storage.go
+++ storage/s3.storage.go
@@ -19,6 +19,7 @@ type S3Uploader struct {
 	BasePath   string
 }
 
+// Upload a file to S3
 func (s *S3Uploader) Upload(file io.Reader, fileHeader *multipart.FileHeader) (string, error) {
 	fileKey := filepath.Join(s.BasePath, fileHeader.Filename)
 
@@ -31,7 +32,6 @@ func (s *S3Uploader) Upload(file io.Reader, fileHeader *multipart.FileHeader) (s
 		Bucket: aws.String(s.BucketName),
 		Key:    aws.String(fileKey),
 		Body:   bytes.NewReader(buf.Bytes()),
-		ACL:    aws.String("public-read"),
 	})
 	if err != nil {
 		return "", fmt.Errorf("failed to upload to S3: %v", err)
@@ -41,6 +41,71 @@ func (s *S3Uploader) Upload(file io.Reader, fileHeader *multipart.FileHeader) (s
 	return fileURL, nil
 }
 
+// GetObject returns a file reader for a specific key
+func (s *S3Uploader) GetObject(key string) (io.ReadCloser, error) {
+	fullKey := filepath.Join(s.BasePath, key)
+	resp, err := s.Client.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(s.BucketName),
+		Key:    aws.String(fullKey),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get object from S3: %v", err)
+	}
+	return resp.Body, nil
+}
+
+// ListObjects lists all objects under the base path
+func (s *S3Uploader) ListObjects() ([]string, error) {
+	resp, err := s.Client.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(s.BucketName),
+		Prefix: aws.String(s.BasePath),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to list objects in S3: %v", err)
+	}
+
+	var keys []string
+	for _, item := range resp.Contents {
+		keys = append(keys, *item.Key)
+	}
+	return keys, nil
+}
+
+// DeleteObject deletes a single object from S3
+func (s *S3Uploader) DeleteObject(key string) error {
+	fullKey := filepath.Join(s.BasePath, key)
+	_, err := s.Client.DeleteObject(&s3.DeleteObjectInput{
+		Bucket: aws.String(s.BucketName),
+		Key:    aws.String(fullKey),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to delete object from S3: %v", err)
+	}
+	return nil
+}
+
+// DeleteMultipleObjects deletes multiple objects from S3
+func (s *S3Uploader) DeleteMultipleObjects(keys []string) error {
+	var objects []*s3.ObjectIdentifier
+	for _, key := range keys {
+		fullKey := filepath.Join(s.BasePath, key)
+		objects = append(objects, &s3.ObjectIdentifier{Key: aws.String(fullKey)})
+	}
+
+	_, err := s.Client.DeleteObjects(&s3.DeleteObjectsInput{
+		Bucket: aws.String(s.BucketName),
+		Delete: &s3.Delete{
+			Objects: objects,
+			Quiet:   aws.Bool(true),
+		},
+	})
+	if err != nil {
+		return fmt.Errorf("failed to delete multiple objects: %v", err)
+	}
+	return nil
+}
+
+// NewS3Uploader initializes the uploader with AWS credentials and config
 func NewS3Uploader(region, accessKey, secretKey, bucketName, basePath string) (*S3Uploader, error) {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
